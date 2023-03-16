22b95e30543287bf0a87c6afd371681fd1881929
veerakalki <veerakalki2001@gmail.com>
2023-03-16 12:08:57 +0530

    conditional statement

--- src/Operator.java
+++ src/Operator.java
@@ -1,6 +1,7 @@
 public class Operator {
     public static void main(String args[]){
         int x=10;
+        //  unary operator
 //        System.out.println(x++);//10 (11)
 //        System.out.println(x);
 //        System.out.println(++x);//12
@@ -17,6 +18,7 @@ public class Operator {
 //        System.out.println(!c);//false (opposite of boolean value)
 //        System.out.println(!d);//true
 //
+// logical and bitwise operator
 //        int a=10;
 //        int b=5;
 //        int c=20;
@@ -37,9 +39,21 @@ public class Operator {
 //        System.out.println(a>b|a++<c);//true | true = true
 //        System.out.println(a);//11 because second condition is checked
 
-        int a=2;
-        int b=5;
-        String min=(a>b)?"a is greater then b":"b is greater then a";
-        System.out.println(min);
+// ternary operator
+//        int a=2;
+//        int b=5;
+//        String min=(a>b)?"a is greater than b":"b is greater than a";
+//        System.out.println(min);
+
+//  assignment operator
+        int a=8;
+        a+=3;//8+3
+        System.out.println(a);
+        a-=4;//11-4
+        System.out.println(a);
+        a*=2;//7*2
+        System.out.println(a);
+        a/=2;//14/2
+        System.out.println(a);
     }
 }
--- src/Student.java
+++ src/Student.java
@@ -1,21 +1,63 @@
 public class Student {
     public static void main(String[] args) {
-        String address = "Delhi, India";
+//        if statements
+//        String address = "Delhi, India";
+//
+//        if (address.endsWith("India")) {
+//
+//            if (address.contains("Chennai")) {
+//                System.out.println("Your city is Chennai");
+//            } else if (address.contains("Noida")) {
+//                System.out.println("Your city is Noida");
+//            } else {
+//                System.out.println(address.split(",")[0]);
+//            }
+//        } else {
+//            System.out.println("You are not living in India");
 
-        if(address.endsWith("India")) {
+//    switch statement
 
-            if(address.contains("Chennai")) {
-                System.out.println("Your city is Chennai");
-            }
-            else if(address.contains("Noida")) {
-                        System.out.println("Your city is Noida");
-                    }
-            else {
-                        System.out.println(address.split(",")[0]);
-                    }
-                }
-        else {
-                    System.out.println("You are not living in India");
-                }
-            }
+//            int num = 2;
+//            switch (num) {
+//                case 0:
+//                    System.out.println("number is 0");
+//                    break;
+//                case 1:
+//                    System.out.println("number is 1");
+//                    break;
+//                default:
+//                    System.out.println(num);
+
+
+// looping statement
+
+//                  int sum = 0;
+//                  for(int j = 1; j<=10; j++) {
+//                     sum = sum + j;
+//                  }
+//                  System.out.println("The sum of first 10 natural numbers is " + sum);
+// for each loop
+
+//        String[] names = {"Java", "C", "C++", "Python", "JavaScript"};
+//        System.out.println("Printing the content of the array names:\n");
+//        for (String name : names) {
+//            System.out.println(name);
+
+//        while loop
+//
+//        int i = 0;
+//        System.out.println("Printing the list of first 10 even numbers \n");
+//        while (i <= 20) {
+//            System.out.println(i);
+//            i = i + 2;
+
+//do while loop
+
+//        int i = 0;
+//        System.out.println("Printing the list of first 10 even numbers \n");
+//        do {
+//            System.out.println(i);
+//            i = i + 2;
+//        }while(i<=10);
         }
+    }
