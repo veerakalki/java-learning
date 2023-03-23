dbc0275e0df00c45f1506545a7411c845813da60
veerakalki <veerakalki2001@gmail.com>
2023-03-23 22:31:32 +0530

    message

---
+++ src/OddOrEven.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class OddOrEven {
+    public static void main(String args[]) {
+        int x = 19;
+        if (x/2==0)
+            System.out.println("true");
+            else;
+           System.out.println("false");
+       }
+    }
+
+
+
+
+
+
