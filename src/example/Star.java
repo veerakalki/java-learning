ffeac786e79c54dec95c546e7d4b38a74ebcbeb3
veerakalki <veerakalki2001@gmail.com>
2023-03-26 21:25:37 +0530

    message

---
+++ src/example/Star.java
@@ -0,0 +1,19 @@
+package example;
+
+public class Star {
+    public static void main(String[] args) {
+        //Code of Java for loop
+        for (int i = 1; i <= 1; i++) {
+            System.out.println("*");
+        }
+        for (int i = 2; i <= 2; i++) {
+            System.out.println("* * *");
+
+        }
+        for (int i = 3; i <= 3; i++) {
+            System.out.print("* * * * *");
+
+
+        }
+    }
+}
