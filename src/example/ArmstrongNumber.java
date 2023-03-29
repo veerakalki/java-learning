be19a994f9dfd7ff599adf634fced2039b5d98b0
veerakalki <veerakalki2001@gmail.com>
2023-03-29 21:42:16 +0530

    message

--- src/example/ArmstrongNumber.java
+++ src/example/ArmstrongNumber.java
@@ -1,22 +1,24 @@
 package example;
 
+import java.util.Scanner;
+
 public class ArmstrongNumber {
     public static void main(String[] args) {
-
-        int number = 123, originalNumber, remainder, result = 0;
-
-        originalNumber = number;
-
-        while (originalNumber != 0)
-        {
-            remainder = originalNumber % 10;
-            result += Math.pow(remainder, 3);
-            originalNumber /= 10;
-        }
-
-        if(result == number)
-            System.out.println(number + " is an Armstrong number.");
-        else
-            System.out.println(number + " is not an Armstrong number.");
+        System.out.println("Enter the numer to check");
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+                scan.close();
+                int original = number;
+                int check=0;
+                int temp=0;
+                while(number>0) {
+                    temp = number % 10;
+                    check = (check * 10) + temp;
+                    number = number / 10;
+                }
+                if(original == check)
+                      System.out.println("it is an Armstrong number.");
+                else
+                      System.out.println("it is not an Armstrong number.");
     }
 }
--- src/example/OddOrEven.java
+++ src/example/OddOrEven.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class OddOrEven {
     public static void main(String args[]) {
-        int x = 19;
-        if (x/2==0)
-            System.out.println("true");
-            else;
-           System.out.println("false");
-       }
-    }
+        int n = 10;
+        System.out.println("odd" + "    " + "even");
+        for (int i = 1;i <= n;i++) {
 
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+            else
+            System.out.print(i + "     ");
+        }
+    }
+}
